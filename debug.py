ec9a51b6d6d6a2acaba70812cf177e557c7f9ce0
justarya <arya@mekari.com>
2020-05-14 21:14:54 +0700

    final

--- debug.py
+++ debug.py
@@ -1,6 +1,10 @@
-from send_email import extract_contacts
+from send_email import extract_contacts, extract_summary, create_template, create_plot
+import os
 import fire
 
 if __name__ == '__main__':
   # Export to Fire
   fire.Fire(extract_contacts)
+  # fire.Fire(extract_summary)
+  # fire.Fire(create_template)
+  fire.Fire(create_plot)
